47361a125cad5546db9eb6eaa06fc67ff2b29879
island843 <3114718317@qq.com>
2025-11-06 17:59:33 +0800

    Add files via upload

---
+++ price.c
@@ -0,0 +1,36 @@
+/* #include<stdio.h>
+ int main()
+ {int price=0;
+ printf("piease print meony");
+ scanf("%d",&price);
+ int change=100-price;
+ printf("give you %d\n",change);
+ return 0;
+ } */
+ /*#include<stdio.h>
+ int main()
+ {int a=0,b=0,c=0;
+ scanf("%d %d",&a,&b);
+ c=a+b;
+ printf("%d",c);
+ return 0;
+ }*/
+ /*#include<stdio.h>
+ float max(float x,float y, float h)
+ {float m=x;
+ if (y>m)m=y;
+ if (h>m)m=h;
+ return m;
+ }
+ int main()
+ {float a=0.0,b=0.0,c=0.0;
+ scanf("%f %f %f",&a,&b,&c);
+ float z=max(a,b,c);
+ printf("%f\n",z);
+  return 0;
+}*/
+/*#include<stdio.h>
+int main()
+{printf("%d\n",10/3 );
+return 0;
+}*/
