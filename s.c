d7a94a6615d9d9abb8d8cc146d818a99601cb2fe
island843 <3114718317@qq.com>
2025-11-11 20:59:26 +0800

    Add files via upload

---
+++ s.c
@@ -0,0 +1,106 @@
+//#include<stdio.h>
+//void main()
+//{
+//	int n;
+//	scanf("%d\n",&n);
+//	printf("\n");
+//	int i;
+//	int a[i];
+//	a[i]=i;
+//	i<=1000&&i>=1;
+//	scanf("%d",a[i]);
+//	for(i=1,i<=n;i++){
+//		
+//		
+//		
+//		
+//	}
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//	
+//}
+//#include<stdio.h>
+//#include<math.h>
+//void main()
+//{
+//	int a,b,c,x,y,z;
+//	scanf("%d %d %d",&a,&b,&c);
+//	
+//	x=sqrt(a*b/c);
+//	y=sqrt(a*c/b);
+//	z=sqrt(b*c/a);
+//	int sum;
+//	sum=(x+y+z)*4;
+//	printf("%d",sum);
+//	
+//}
+//#include<stdio.h>
+//int main()
+//{
+//	int a[3];
+//	int i;
+//	a[i]=i+1;
+//	scanf("%d %d %d",&a[1],&a[2],&a[3]);
+//	printf("%d %d %d",a[1],a[2],a[3]);
+//	return 0;
+//	
+//}
+
+//#include <stdio.h>
+//int min()
+//{int a,b;
+// if(a<b){
+//     printf("%d",a);
+// }else if(a>b){
+//     printf("%d",b);
+// }
+//    
+//}
+//int main()
+//{
+//    int n;
+//    //scanf("%d",&n);
+//    n=3;
+//    int a[n];
+//    a[n]=n+1;
+//    scanf("%d",&a[n]);
+//    int x=0,y=0,z;
+//    for(x=1;x<n;x++){
+//    	y=a[n]-a[x];
+//       
+//        printf("%d",y);
+//        
+//    }
+//    z=min(y=a[n]-a[x]);
+//    printf("%d",z);
+//    
+//    return 0;
+//    
+//}
+#include<stdio.h>
+int main()
+{
+    char*s1();
+    char*s2();
+    char*s3();
+    scanf("%c*",&s1);
+    scanf("%c*",&s2);
+    scanf("%c*",&s3);
+    if(s1==s3){
+        printf("%c*",s1);
+        
+    }
+    if(s1!=s3){
+        printf("abcdabc");
+        
+    }
+    
+    
+    
+    
+}
